f600fc64fcc577dfdb1142e50717605a566bac4d
mazhenyu997 <mazhenyu997@foxmail.com>
2018-08-19 17:06:20 +0800

    图片验证码和短信验证码

---
+++ meiduo_mall/meiduo_mall/apps/verifications/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from django_redis import get_redis_connection
+
+
+class CheckImageCodeSerializer(serializers.Serializer):
+    # 图片验证码序列化器
+    image_code_id = serializers.UUIDField()
+    text = serializers.CharField(min_length=4, max_length=4)
+
+    def validate(self, attrs):
+        # 校验图片验证码是否正确， 查询redis数据库获取真实的验证码
+        image_code_id = attrs['image_code_id']
+        text = attrs['text']
+        redis_conn = get_redis_connection('verify_codes')
+
+        real_image_id = redis_conn.get('img_%s' %image_code_id)
+
+        if real_image_id is None:
+            # 真实验证码不存在或者过期
+            raise serializers.ValidationError("无效的图片验证码")
+
+        # 对比  python3 redis返回的是bytes类型的值
+        real_image_id = real_image_id.decode()
+        if real_image_id.lower() != text.lower():
+            raise serializers.ValidationError("图片验证码错误")
+
+        # redis中发送短信验证码的标志，油reids维护60S的时间
+        mobile = self.context['view'].kwargs['mobile']
+
+        send_flag = redis_conn.get('send_flag%s' % mobile )
+
+        if send_flag:
+            raise serializers.ValidationError("发送短信过于频繁")
+
+        return attrs
+
+
+
+
+
+
+
--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -6,6 +6,11 @@ from rest_framework.views import APIView
 from meiduo_mall.libs import constants
 from meiduo_mall.libs.captcha.captcha import captcha
 from django.http.response import HttpResponse
+from rest_framework.generics import GenericAPIView
+from . import serializers
+import random
+from meiduo_mall.libs.yuntongxun.sms import CCP
+from rest_framework.response import Response
 
 
 class ImageCodeView(APIView):
@@ -25,4 +30,31 @@ class ImageCodeView(APIView):
         redis_conn.setex("img_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
 
         return HttpResponse(image, content_type="images/jpg")
+
+
+class SMSCodeView(GenericAPIView):
+    serializer_class = serializers.CheckImageCodeSerializer
+
+    def get(self, request, mobile):
+        serializer = self.get_serializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+
+        # 校验通过，生成短信验证码
+        sms_code = '%06d' % random.randint(0, 999999)
+
+        # 保存验证码及发送记录
+        redis_conn = get_redis_connection('verify_codes')
+        redis_conn.setex("sms_%s" % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+        redis_conn.setex("send_flag_%s" % mobile, constants.SMS_SEND_INTERVAL, 1)
+
+        # 发送短信
+        ccp = CCP()
+
+        sms_time = str(constants.SMS_CODE_REDIS_EXPIRES / 60)
+        ccp.send_template_sms(mobile, [sms_code, sms_time], constants.SMS_CODE_TEMPLATE_ID)
+
+        return Response[{"message": "ok"}]
+    
+
+
 
